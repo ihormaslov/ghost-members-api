3a761b6b33f30c83455a78d0db1e1f1b989830d3
Ihor Maslov <maslov.ihor@gmail.com>
2020-01-21 13:15:50 +0200

    login

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const {Router} = require('express');
 const body = require('body-parser');
 const MagicLink = require('@tryghost/magic-link');
@@ -115,7 +116,9 @@ module.exports = function MembersApi({
         sendMagicLink: Router(),
         createCheckoutSession: Router(),
         handleStripeWebhook: Router(),
-        updateSubscription: Router({mergeParams: true})
+        updateSubscription: Router({mergeParams: true}),
+        handleLogin: Router(),
+        handleCreateAccount: Router(),
     };
 
     middleware.sendMagicLink.use(body.json(), async function (req, res) {
@@ -281,6 +284,57 @@ module.exports = function MembersApi({
 
         res.writeHead(204);
         res.end();
+    });
+
+    middleware.handleLogin.use(body.json(), async function (req, res) {
+        const email = req.body.email;
+        const password = req.body.password;
+        if (!email && !password) {
+            res.writeHead(400);
+            return res.end('Bad Request.');
+        }
+
+        const member = await getMemberIdentityData(email);
+        if (!member) {
+            res.writeHead(400);
+            return res.end('Bad Request.');
+        }
+
+        if (member.password === password){
+            const token = jwt.sign({}, secret, {
+                algorithm: 'HS256',
+                subject: email,
+                expiresIn: '10m'
+            }); 
+            return res.redirect('/?token='+ token + '&action=signin')
+        }
+        
+        res.writeHead(400);
+        return res.end('Wrong password.')
+    });
+
+    middleware.handleCreateAccount.use(body.json(), async function (req, res) {
+        const email = req.body.email;
+        const password = req.body.password;
+        if (!email && !password) {
+            res.writeHead(400);
+            return res.end('Bad Request.');
+        }
+
+        const member = users.create({email, password}).catch((error) => {
+            return Promise.reject(Error(error));
+        })
+
+        const token = jwt.sign({}, secret, {
+            algorithm: 'HS256',
+            subject: email,
+            expiresIn: '10m'
+        });
+
+        // res.writeHead(201);
+        // return res.end('Created.');
+        // const url = this.getSigninURL(token, 'signin');
+        return res.redirect('/?token='+ token + '&action=signup')
     });
 
     const getPublicConfig = function () {
